d39578796a7fc4cd8887173e78e2a51c93cc0cb8
mark-saint <markmphillip@gmail.com>
2023-05-16 00:37:44 +0300

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -6,27 +6,35 @@ This is a base file
 
 import uuid
 import datetime
+
+
 class BaseModel:
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         this is init
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now().isoformat()
-        self.updated_at = datetime.datetime.now().isoformat()
+        if kwargs:
+            self.__dict__ = kwargs
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(BaseModel.__name__, (self.id), self.__dict__)
+        return "[{}] ({}) {}".format(BaseModel.__name__, (self.id),
+                                     self.__dict__)
 
     def save(self):
         """
         updates the public instance attribute updated_at with current datetime
         """
-        BaseModel.updated_at = datetime.datetime.now().isoformat()
+        BaseModel.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """
-        returns the dict containing all keys and values of __dict__ of the instance
+        returns the dict containing all keys and
+        values of __dict__ of the instance
         """
         return self.__dict__
