e89470826b38dbb29e87a2d88c974111b983b5b3
mark-saint <markmphillip@gmail.com>
2023-05-16 23:47:17 +0300

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,17 +21,29 @@ class FileStorage:
         self.__objects = {}
 
     def all(self):
+        """
+        Get all the objects
+        """
         return self.__objects
 
     def new(self, obj):
+        """
+        create a new object
+        """
         self.__objects.update({"{}.{}".format(obj["__class__"],
                                               obj["id"]): obj})
 
     def save(self):
+        """
+        save the object
+        """
         with open(self.__file_path, 'w') as f:
             json.dump(self.__objects, f)
 
     def reload(self):
+        """
+        reload the object
+        """
         if os.path.isfile(self.__file_path):
             with open(self.__file_path) as f:
                 self.__objects = json.load(f)
