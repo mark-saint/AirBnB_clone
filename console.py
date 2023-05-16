32133406561af479b3d8f2900020eebe998f71b3
mark-saint <markmphillip@gmail.com>
2023-05-17 00:03:50 +0300

    console.py

---
+++ console.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This is a cmd file
+"""
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    the class
+    """
+    
+    prompt = '(hbnb) '
+    def do_quit(self, line):
+        """
+        Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, line):
+        """
+        exit
+        """
+        return True
+
+    def help(self):
+        pass
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
