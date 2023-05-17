92d539c2144e33f66c19c5e3d216b05e3ea9eacc
mark-saint <markmphillip@gmail.com>
2023-05-18 01:37:23 +0300

    console.py

--- console.py
+++ console.py
@@ -8,6 +8,11 @@ import cmd
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -17,7 +22,13 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
 
-    classes = {"BaseModel": BaseModel, "User": User}
+    classes = {"BaseModel": BaseModel,
+               "User": User,
+               "Place": Place,
+               "State": State,
+               "City": City,
+               "Amenity": Amenity,
+               "Review": Review}
 
     def do_quit(self, line):
         """
